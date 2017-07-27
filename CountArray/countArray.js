6ea2fc38b241317239abb028211f10683083c493
william rulon <Rulonwilliam@gmail.com>
2017-07-26 21:04:46 -0400

    compacting everything

---
+++ CountArray/countArray.js
@@ -0,0 +1,25 @@
+/**
+
+The Challenge
+
+Given an array of strings, return the count of the number of strings with the given length.
+
+wordsCount(["a", "bb", "b", "ccc"], 1) ? 2
+wordsCount(["a", "bb", "b", "ccc"], 3) ? 1
+wordsCount(["a", "bb", "b", "ccc"], 4) ? 0
+
+**/
+var countArray = (function () {
+    function countArray() {
+    }
+    return countArray;
+}());
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
---
+++ CountArray/countArray.ts
@@ -0,0 +1,27 @@
+﻿/**
+
+The Challenge
+
+Given an array of strings, return the count of the number of strings with the given length.
+
+wordsCount(["a", "bb", "b", "ccc"], 1) ? 2
+wordsCount(["a", "bb", "b", "ccc"], 3) ? 1
+wordsCount(["a", "bb", "b", "ccc"], 4) ? 0
+
+**/
+
+class countArray {
+
+    arrayCount: number;
+
+}
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
